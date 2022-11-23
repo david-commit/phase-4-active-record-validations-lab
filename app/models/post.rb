98ab3c33ed15184ba738f7152bd68ef32fa1d38e
david-commit <david.ondiege@students.moringaschool.com>
2022-11-23 10:43:24 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+ validates :title, presence: true
+ validates :content, length: { minimum: 250}
+ validates :summary, length: { maximum: 250}
+ validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+ validate :clickbait_title
+ TITLE_NOT_CONTAIN = [
+     /Won't Believe/i,
+     /Secret/i,
+     /Top/i,
+     /Guess/i
+
+ ] 
+
+ def clickbait_title
+    if TITLE_NOT_CONTAIN.none? {|word| word.match title}
+     errors.add(:title, "Invalid title")
+    end
+ end
 end
